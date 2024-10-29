a210de2e92a8d365a7d4d9008f8f094cd3664de0
Mark Ferianec <mjf397@gmail.com>
2024-10-29 04:17:12 -0600

    Create messages array and display in index view

--- app.js
+++ app.js
@@ -6,9 +6,26 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { messages: messages });
 });
+
+// app.get("/new", (req, res) => {
+//   res.send("New message form");
+// });
 
 const PORT = 3000;
 
