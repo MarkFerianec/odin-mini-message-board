01e1832a2940bbffb032b3f6d0d03a0c3f360072
Mark Ferianec <mjf397@gmail.com>
2025-03-18 06:18:30 -0600

    Add sanitization and validation to form

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -5,6 +5,8 @@ const links = [
   { href: "new", text: "Form" },
 ];
 
+const { body, validationResult } = require("express-validator");
+
 exports.getIndex = async (req, res) => {
   const messagesFromDB = await db.getAllMessages();
 
@@ -23,11 +25,40 @@ exports.getMessageDetails = async (req, res) => {
   res.render("messagedetails", { message: message.rows[0], links: links });
 };
 
-exports.postNewMessage = async (req, res) => {
-  const { message, username } = req.body;
-  const added = new Date().toString();
+const validateUsername = [
+  body("username")
+    .trim()
+    .isLength({ min: 3, max: 15 })
+    .withMessage("Name must be between 3 to 15 characters")
+    .escape(),
+];
 
-  await db.postNewMessageToDb(message, username, added);
+const validateMessage = [
+  body("message")
+    .trim()
+    .isLength({ min: 4, max: 30 })
+    .withMessage("Message must be between 4 to 30 characters")
+    .escape(),
+];
 
-  res.redirect("/");
-};
+exports.postNewMessage = [
+  validateUsername,
+  validateMessage,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      const { username, message } = req.body;
+      const added = new Date().toString();
+
+      await db.postNewMessageToDb(message, username, added);
+
+      res.redirect("/");
+    } else {
+      res.render("form", {
+        errors: errors.array(),
+        links: links,
+      });
+    }
+  },
+];
